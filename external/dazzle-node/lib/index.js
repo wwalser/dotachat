21c607fd9f806dea6d397e64dd413347a13a7f56
Joshua Ali <jali@atlassian.com>
2013-09-11 11:59:07 +1000

    Resolve usernames rather than having hardcoded aliases

--- external/dazzle-node/lib/index.js
+++ external/dazzle-node/lib/index.js
@@ -1,3 +1,4 @@
 var Dota2Client = require('./dota2-client');
-var steamClient = require('./steam-client');
-module.exports = exports = {dota: Dota2Client, steam: steamClient};
+var steamClient1 = require('./steam-client-1');
+var steamClient2 = require('./steam-client-2');
+module.exports = exports = {dota: Dota2Client, steam1: steamClient1, steam2: steamClient2};
