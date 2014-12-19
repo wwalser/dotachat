551764a83af163e44808e63448ea3e08ab3e8504
Wesley Walser <wwalser@atlassian.com>
2014-12-20 02:51:25 +1100

    Hypens are allowed in paths.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
  * Response to hipchat messages.
  */
 module.exports = function(request, response) {
-	var controllerName = request.params.controller.match(/[0-9a-zA-Z]*/)[0],
+	var controllerName = request.params.controller.match(/[0-9a-zA-Z\-]*/)[0],
 		controller;
 	
 	if (controllerName.length > 0) {
