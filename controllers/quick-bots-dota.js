f5097a067a4b3c9afcfce46ce29ae8cb9e94900b
Wesley Walser <wwalser@atlassian.com>
2014-12-20 23:22:00 +1100

    Respond to quick-bots with information about trouble if we hit some.

--- controllers/quick-bots-dota.js
+++ controllers/quick-bots-dota.js
@@ -11,5 +11,8 @@ module.exports = function(request, response) {
         } catch (e) {
             console.log('uh oh', e);
         }
+    }, function(messageObject){
+        console.log("Letting quick-bots know there was trouble.")
+        response.json(messageObject)
     });
 };
