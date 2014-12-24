b8d2e7067aa1410f57c99cf2fc556493a48652f9
Wesley Walser <wwalser@atlassian.com>
2014-12-24 12:32:08 +1100

    Added translation bot for quickbots.

---
+++ controllers/quick-bots-translations.js
@@ -0,0 +1,17 @@
+var translations = require('../lib/translations');
+
+module.exports = function(request, response) {
+    var message = request.body.full || request.body.text;
+    console.log("Translating: " + message);
+
+    translations(message).then(function(messageObject){
+        try {
+            response.json(messageObject);
+        } catch (e) {
+            console.log('A problem sending the response from translation bot.', e);
+        }
+    }, function(messageObject){
+        console.log("Letting quick-bots know there was trouble.")
+        response.json(messageObject)
+    });
+};
